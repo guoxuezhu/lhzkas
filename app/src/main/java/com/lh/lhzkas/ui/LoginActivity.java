cbd930ec8e8e900fa8485fb0a644eea540aeb856
gxz <1174327885@qq.com>
2019-01-25 14:16:43 +0800

    串口

--- app/src/main/java/com/lh/lhzkas/ui/LoginActivity.java
+++ app/src/main/java/com/lh/lhzkas/ui/LoginActivity.java
@@ -6,7 +6,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.lh.lhzkas.R;
+import com.lh.lhzkas.utils.ELog;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android_serialport_api.SerialPort;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -18,6 +24,9 @@ public class LoginActivity extends BaseActivity {
     EditText login_name;
     @BindView(R.id.login_password)
     EditText login_password;
+    private SerialPort serialPort1;
+    private InputStream inputStream1;
+    private boolean threadStatus1 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,44 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+
+        try {
+            serialPort1 = new SerialPort(new File("/dev/ttyS1"), 9600, 0);
+            //获取打开的串口中的输入输出流，以便于串口数据的收发
+            inputStream1 = serialPort1.getInputStream();
+//            outputStream1 = serialPort1.getOutputStream();
+            threadStatus1 = true;
+            new ReadThread().start(); //开始线程监控是否有数据要接收
+        } catch (IOException e) {
+            ELog.e("======open_ck=====打开串口异常");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * 单开一线程，来读数据
+     */
+    private class ReadThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            //判断进程是否在运行，更安全的结束进程
+            byte[] buffer = new byte[1024];
+            int size; //读取数据的大小
+            try {
+                while (threadStatus1 && (size = inputStream1.read(buffer, 0, 1024)) > 0) {
+                    if (size > 0) {
+                        ELog.i("=========run: 接收到了数据=======" + new String(buffer, 0, size));
+                        ELog.i("=========run: 接收到了数据大小=====" + size);
+                    }
+                }
+
+            } catch (IOException e) {
+                ELog.i("=========run: 数据读取异常========" + e.toString());
+            }
+        }
     }
 
 
@@ -52,6 +99,7 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        threadStatus1 = false;
     }
 
 
